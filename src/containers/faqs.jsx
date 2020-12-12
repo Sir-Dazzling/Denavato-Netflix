f8ff18115717cd3a10a3b16f71ea0c5842190395
Sir-Dazzling <ottidan20@gmail.com>
2020-12-12 08:00:49 +0100

    finished opt form

--- src/containers/faqs.jsx
+++ src/containers/faqs.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Accordion} from '../components';
+import {Accordion, OptForm} from '../components';
 import faqsData from '../fixtures/faqs.json';
 
 export function FaqsContainer()
@@ -7,15 +7,20 @@ export function FaqsContainer()
     return (
         <Accordion>
             <Accordion.Title>Frequently Asked Questions</Accordion.Title>
-            {faqsData.map((item) => (
-                <Accordion.Item key={item.id}>
-                    <Accordion.Header>{item.header}</Accordion.Header>
-                    <Accordion.Body>{item.body}</Accordion.Body>
-                </Accordion.Item>
-            ))}
             <Accordion.Frame>
-
-            </Accordion.Frame>
+                {faqsData.map((item) => (
+                    <Accordion.Item key={item.id}>
+                        <Accordion.Header>{item.header}</Accordion.Header>
+                        <Accordion.Body>{item.body}</Accordion.Body>
+                    </Accordion.Item>
+                ))}
+            </Accordion.Frame> 
+            <OptForm>
+                <OptForm.Text>Ready to watch? Enter your email to create or restart your membership.</OptForm.Text>
+                <OptForm.Input placeholder="Email address" />
+                <OptForm.Button>Try it now</OptForm.Button>
+                <OptForm.Break />
+            </OptForm>
         </Accordion>
     );
 }
