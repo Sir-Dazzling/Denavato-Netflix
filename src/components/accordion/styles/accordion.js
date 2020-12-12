3558e0ffe2055fd0f1c471878e7ac4050e95175a
Sir-Dazzling <ottidan20@gmail.com>
2020-12-12 05:11:03 +0100

    added libraries

---
+++ src/components/accordion/styles/accordion.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Frame = styled.div``;
+
+export const Inner = styled.div``;
+
+export const Header = styled.h1``;
+
+export const Item = styled.div``;
+
+export const Title = styled.h1``;
+
+export const Body = styled.div``;
