0ba5a6bdf7eb2b16e35d9caccdcfb30338eff710
Sir-Dazzling <ottidan20@gmail.com>
2021-01-09 17:20:46 +0100

    integrated firebase

---
+++ src/utilities/firebase.prod.js
@@ -0,0 +1,17 @@
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export {firebase};
