1e8fe6d7a2e8124c59f14b3fad85ab7820d37b0f
Sir-Dazzling <ottidan20@gmail.com>
2021-01-10 06:52:47 +0100

    finished login functionality

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,7 +1,69 @@
-import React from 'react';
+import {useState, useContext} from 'react';
+import {useHistory} from 'react-router-dom';
+import {FirebaseContext} from '../context/firebase';
+import {HeaderContainer} from '../containers/header';
+import {FooterContainer} from '../containers/footer';
+import {Form} from '../components';
+import * as ROUTES from '../constants/routes';
 
 export default function Signin(){
+    const history = useHistory();
+    const {firebase} = useContext(FirebaseContext);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+
+    // Checking if form is valid
+    const isInvalid = password === "" || email === "";
+
+    // Sign in action handler
+    const handleSignIn = async(event) => {
+        event.preventDefault();
+        await firebase
+            .auth()
+            .signInWithEmailAndPassword(email, password)
+            .then(() => {
+                // push to the browse page
+                history.push(ROUTES.BROWSE);
+            })
+            .catch((error) => {
+                setEmail("");
+                setPassword("");
+                setError(error.message);
+            })
+    };
+
     return (
-        <p>Hello this is the Sign in page!</p>
+        <>
+            <HeaderContainer>
+                <Form>
+                    <Form.Title>Sign In</Form.Title>
+                    {error && <Form.Error>{error}</Form.Error>}
+                    <Form.Base onSubmit={handleSignIn} method="POST">
+                        <Form.Input
+                         placeholder="Email Address"
+                         value={email}
+                         onChange={({target}) => setEmail(target.value)}/>
+                        <Form.Input
+                         type="password"
+                         autoComplete="off"
+                         placeholder="Password"
+                         value={password}
+                         onChange={({target}) => setPassword(target.value)}/>
+                         <Form.Submit disabled={isInvalid} type="submit">Sign In</Form.Submit>
+                    </Form.Base>
+
+                    <Form.Text>
+                        New to Netflix? <Form.Link to="/signup">Sign up now</Form.Link>
+                    </Form.Text>
+
+                    <Form.TextSmall>
+                        This page is protected byt Google reCAPTCHA to ensure you are not a bot. Learn more.
+                    </Form.TextSmall>
+                </Form>
+            </HeaderContainer>
+            <FooterContainer>
+            </FooterContainer>
+        </>
     );
 }
--- src/utilities/firebase.prod.js
+++ src/utilities/firebase.prod.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: process.env.APIKEY,
-    authDomain: process.env.AUTHDOMAIN,
-    projectId: process.env.PROJECTID,
-    storageBucket: process.env.STORAGEBUCKET,
-    messagingSenderId: process.env.MESSAGINGSENDERID,
-    appId: process.env.APPID,
-    measurementId: process.env.MEASUREMENTID
+    apiKey: "AIzaSyCOx8eLoxTpcH2H42slunr1Ww6XFHhLPcc",
+    authDomain: "dazzling-netflix-demo.firebaseapp.com",
+    projectId: "dazzling-netflix-demo",
+    storageBucket: "dazzling-netflix-demo.appspot.com",
+    messagingSenderId: "671200860892",
+    appId: "1:671200860892:web:720ce6f634dfa7e6127219",
+    measurementId: "G-RNRGN1LSEC"
 };
 
 const firebase = Firebase.initializeApp(config);
